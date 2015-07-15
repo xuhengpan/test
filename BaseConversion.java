56e336d4abd5a2ba62ed1db68f6dbc653fe107fe
xuhengpan <xuhengpan@126.com>
2015-07-15 16:52:12 +0800

    test
    test

---
+++ BaseConversion.java
@@ -0,0 +1,22 @@
+package selectsort;
+
+import java.util.Stack;
+
+public class BaseConversion {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		int i = 2007;
+		baseConversion(i);
+	}
+
+	public static void baseConversion(int i) {
+		Stack s = new Stack();
+		while (i > 0) {
+			s.push(i % 8 + "");
+			i = i / 8;
+		}
+		while (!s.isEmpty())
+			System.out.print((String) s.pop());
+	}
+}
